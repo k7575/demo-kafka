881cf9cb256127d627f1e09f5c10593e827b7b18
k7575 <vs.00000000000000000@gmail.com>
2024-06-17 17:49:21 +0300

    dbg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rdkafka::consumer::{Consumer, StreamConsumer};
+use rdkafka::message::{Header, Headers, OwnedHeaders};
 use rdkafka::producer::{FutureProducer, FutureRecord};
 use rdkafka::util::Timeout;
 use rdkafka::{ClientConfig, Message};
@@ -12,7 +13,7 @@ async fn main() {
 
 async fn producer() {
     let p = ClientConfig::new()
-        .set("bootstrap.servers", "localhost:9092")
+        .set("bootstrap.servers", "192.168.56.3:9092")
         .set("queue.buffering.max.ms", "0")
         .create::<FutureProducer>()
         .expect("Failed to create client");
@@ -20,7 +21,12 @@ async fn producer() {
     loop {
         let message = &format!("Message: {}", chrono::Local::now().to_string());
         p.send(
-            FutureRecord::<(), _>::to("test").payload(message),
+            FutureRecord::<(), _>::to("test")
+                .payload(message)
+                .headers(OwnedHeaders::new().insert(Header {
+                    key: "Custom",
+                    value: Some("Ok"),
+                })),
             Timeout::Never,
         )
         .await
@@ -32,16 +38,23 @@ async fn producer() {
 
 async fn consumer() {
     let c = ClientConfig::new()
-        .set("bootstrap.servers", "localhost:9092")
+        .set("bootstrap.servers", "192.168.56.3:9092")
         .set("enable.partition.eof", "false")
         .set("group.id", "test_group_1")
         .set("client.id", "test_client_1")
+        //.set("enable.auto.commit", "false")
+        //.set("enable.auto.offset.store", "false")
         .create::<StreamConsumer>()
         .expect("Failed to create client");
 
     c.subscribe(&["test"]).unwrap();
     loop {
         let message = c.recv().await.unwrap();
+        dbg!(message.topic());
+        dbg!(message.partition());
+        dbg!(message.offset());
+        dbg!(message.timestamp());
+        dbg!(message.headers().unwrap().get(0));
         let data = message.payload().unwrap().to_vec();
         println!("Consumer read: {}", unsafe {
             String::from_utf8_unchecked(data)
